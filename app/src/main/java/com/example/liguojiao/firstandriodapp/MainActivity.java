2c93644ca887d8df7285de884961b55fdaa31df8
Liguo Jiao <liguo.jiao@mckay.co.nz>
2016-04-10 22:41:43 +1200

    first commit

---
+++ app/src/main/java/com/example/liguojiao/firstandriodapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.liguojiao.firstandriodapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.graphics.Color;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+    public void startTimer(View view){
+
+    }
+
+    public void setupTimer(View view){
+        startActivity(new Intent(MainActivity.this,TimerSetup.class));
+    }
+}
---
+++ app/src/main/java/com/example/liguojiao/firstandriodapp/TimerSetup.java
@@ -0,0 +1,13 @@
+package com.example.liguojiao.firstandriodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_setup);
+    }
+}
